9bbd02843d6f5aff109f42ded7eeed4fcdfdd611
Mhijazi16 <gultimate0@gmail.com>
2024-10-17 22:12:30 +0300

    add fill_package() method

--- crawler.py
+++ crawler.py
@@ -20,3 +20,32 @@ def get_package():
             'conflictors': [],
             'dependencies': []
         }
+
+def fill_package(name: str):
+
+    package = get_package()
+    headers = ["Name","Size","Packager","Description","URL"]
+    data = os.popen(f"pacman -Qi {name}").read()
+    lines = data.strip().split('\n')
+
+    for line in lines: 
+        key, value = line.split(":",1)
+        key = key.strip()
+        value = value.strip()
+
+        if key in headers:  
+            package[key] = value
+        elif key == "Depends On":
+            dependencies = value.split('  ')
+            for dep in dependencies:
+                if ">" in dep:
+                    result = dep.split('>',1)[0]
+                    package['dependencies'].append(result)
+                else: 
+                    package['dependencies'].append(dep)
+        elif key == "Conflicts With": 
+            package['conflictors'].append(value)
+
+    return package
+
+print(fill_package("glibc"))
