7250f9d3572f640527724f0d785784fd8764dafc
Mhijazi16 <gultimate0@gmail.com>
2024-11-24 19:16:49 +0200

    add create_depend_on_relation() query

--- queries.py
+++ queries.py
@@ -23,3 +23,12 @@ def label_as_leaf(tx, package):
     result = tx.run("""MERGE (p:Package{name: $name})
                        SET p:Leaf""", 
                     name=package["Name"])
+
+def create_depend_on_relation(tx, name, dep):
+    result = tx.run("""
+                    MERGE (p:Package {name: $name})
+                    MERGE (p2:Package {name: $dep_name})
+                    SET p2:DEPENDENCY
+                    MERGE (p)-[d:DEPENDS_ON]->(p2)
+                    """, 
+                    name=name, dep_name=dep)
