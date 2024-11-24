91025ccf58b4af5d61145e5bc0f1d0c44264423a
Mhijazi16 <gultimate0@gmail.com>
2024-11-24 19:15:14 +0200

    adding queries

---
+++ queries.py
@@ -0,0 +1,15 @@
+
+def create_package(tx, package):
+    result = tx.run("""
+            MERGE (p:Package{
+                name: $name, url: $url, version: $version, owner: $owner, 
+                description: $description, arch: $arch, size: $size})
+            RETURN p.name AS name """, 
+
+             name=package["Name"], 
+             version=package["Version"],
+             url=package["URL"],
+             owner=package["Packager"],
+             arch=package["Architecture"],
+             size=package["Installed Size"],
+             description=package["Description"])
