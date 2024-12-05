8b8b73d2721fc3f07fd5f22f54fa78d5effcb00f
Mhijazi16 <gultimate0@gmail.com>
2024-12-05 12:08:59 +0200

    add project_graph() method

--- models.py
+++ models.py
@@ -1,18 +1,22 @@
 from neo4j import GraphDatabase 
-from queries import create_package, create_conflicts_with_relation, create_depend_on_relation, label_as_leaf, label_as_library
+from queries import create_package
+from queries import create_conflicts_with_relation
+from queries import create_depend_on_relation
+from queries import label_as_leaf
+from queries import label_as_library
+from queries import project_graph
 import os
 
 def get_package_template():
     return {
-            'URL': '',
-            'Name': '',
-            'Version':'',
+            'URL': '', 'Name': '', 'Version':'',
             'Packager': '',
             'Description': '',
             'Architecture': '',
             'Installed Size': '', 'conflictors': [],
             'dependencies': [],
             'isLibrary': False,
+            'Manual': [float],
         }
 
 PASS = os.environ["NEO4J_PASS"]
@@ -61,3 +65,26 @@ def store_package(package):
                                   conflict)
     except Exception as e : 
         print(f"something went wrong!! : {e}")
+
+def get_topological_sort(name):
+    try: 
+        driver = GraphDatabase.driver(URI, auth=AUTH)
+        driver.verify_connectivity()
+
+        with driver.session(database="neo4j") as session: 
+            session.execute_write(project_graph, name)
+
+        result = driver.execute_query("""
+            CALL gds.dag.topologicalSort.stream($name, {computeMaxDistanceFromSource: true})
+            YIELD nodeId, maxDistanceFromSource
+            RETURN gds.util.asNode(nodeId).name AS name, maxDistanceFromSource AS hops
+            ORDER BY hops DESC , name 
+        """, name=name)
+
+        for record in result.records:
+            print(record['name'])
+        input()
+    except Exception as e : 
+        print(f"something went wrong!! : {e}")
+
+get_topological_sort('fastfetch')
