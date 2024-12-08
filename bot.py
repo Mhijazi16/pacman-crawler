aa257b1bf9c60a520c9ae47e4da861f0e44ab4f3
Mhijazi16 <gultimate0@gmail.com>
2024-12-08 12:45:00 +0200

    adding a simple bot

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from langchain_ollama.chat_models import ChatOllama
 from langgraph.prebuilt import create_react_agent
-from models import query_graph
+from models import get_all_dependencies, get_package_info, get_dependency_by_distance, get_topological_sort
 from crawler import crawl
 
 
-tools = [query_graph, crawl]
+tools = [get_package_info, get_dependency_by_distance, crawl, get_all_dependencies, get_topological_sort]
 model = ChatOllama(model="llama3.1", temperature=0).bind_tools(tools)
 agent = create_react_agent(model=model, tools=tools)
 print("Hello I'm an Interactive Package Manager \nEnter your prompt down here ")
